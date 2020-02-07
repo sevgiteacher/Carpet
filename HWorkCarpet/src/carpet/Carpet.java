a53c87d85874de18a99cd97805110817720c2455
sevgiteacher <58793691+sevgiteacher@users.noreply.github.com>
2020-02-07 19:54:59 +0000

    Add files via upload
    Carpet project has been created.

---
+++ HWorkCarpet/src/carpet/Carpet.java
@@ -0,0 +1,20 @@
+package carpet;
+
+public class Carpet {
+	
+	private double cost;
+
+	public Carpet(double cost) {
+		if (cost<0) {
+			cost = 0;
+		}
+		this.cost = cost;
+	}
+	
+	public double getCost() {
+		return cost;
+	}
+	
+	
+
+}
